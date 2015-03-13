f5499f640221338ee4ebe995ad23075772720231
John Fraboni <johnfraboni@hotmail.com>
2015-03-13 19:19:24 +0000

    * add root

--- js/opspark.js
+++ js/opspark.js
@@ -1,6 +1,9 @@
 (function (window) {
     window.opspark = window.opspark || {};
     
+    var 
+        createjs = window.createjs;
+        
     window.opspark.makeApp = function (updateable) {
         var 
             _stage, 
@@ -8,8 +11,8 @@
             _updateable, 
             _app;
         
-        _stage  = new createjs.Stage(canvas);
         _canvas = document.getElementById('canvas');
+        _stage  = new createjs.Stage(_canvas);
         _updateable = (updateable) ? [].concat(updateable) : [];
         
         _app = {
@@ -29,29 +32,32 @@
                 }
                 return _app;
             },
-
+            
             update: function(e) {
+                _stage.update();
                 for (var i = 0; i < _updateable.length; i++) {
                     _updateable[i].update();
                 }
-                // always update the stage last //
-                _stage.update();
             }
         };
         
+        window.addEventListener('resize', onResize, false);
+        function onResize(e) {
+            setSize();
+            _app.update(e);
+        }
         
-        window.addEventListener('resize', resizeCanvas, false);
-        function resizeCanvas(e) {
+        function setSize() {
             _canvas.width = window.innerWidth;
             _canvas.height = window.innerHeight;
-            if (e) { _app.update(e) }
         }
-        resizeCanvas();
+        setSize();
         
-        //_app.stage.addChild(draw.rect(canvas.width, canvas.height, null, '#4F5661', 1));
         _app.stage.addChild(_app.view);
         createjs.Ticker.setFPS(60);
         createjs.Ticker.on('tick', _app.update);
+        
+        // createjs.Touch.enable(_canvas, true, false);
 
         return _app;
     };
